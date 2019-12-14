4c93734357ab8ee2c0fbf94ef313437a0c3f6af3
takonasu <piano88_viola_tako8@hotmail.co.jp>
2019-12-14 23:24:45 +0900

    バージョン

--- Twinte/ViewController.swift
+++ Twinte/ViewController.swift
@@ -91,7 +91,6 @@ class ViewController: UIViewController, WKUIDelegate,WKNavigationDelegate  {
         if let host : String = navigationAction.request.url?.host{
             // グローバル変数に格納
             g_Url = navigationAction.request.url
-            print(host)
             if(host == "app.twinte.net" || host == "dev.api.twinte.net" || host == "appleid.apple.com"){//この部分を処理したいURLにする
                 decisionHandler(WKNavigationActionPolicy.allow)
             }else{
