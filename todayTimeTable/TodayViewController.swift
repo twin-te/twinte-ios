7db9e718a99a9815ffddf87302160b4569950922
takonasu <piano88_viola_tako8@hotmail.co.jp>
2020-01-02 02:04:21 +0900

    イベント情報取得（バグあり）

--- todayTimeTable/TodayViewController.swift
+++ todayTimeTable/TodayViewController.swift
@@ -9,11 +9,28 @@
 import UIKit
 import NotificationCenter
 
+// 授業情報を格納
 struct Lecture: Codable {
     let period: Int
     let room: String
     let lecture_name: String
     let instructor: String
+}
+
+// イベント情報格納
+struct OutputSchoolCalender: Codable {
+    let module:String?
+    let event:event?
+    let substituteDay:substituteDay?
+    
+    struct event: Codable{
+        let description:String
+        let event_type:String
+    }
+    
+    struct substituteDay: Codable{
+        let change_to:String
+    }
 }
 
 struct LectureGet {
@@ -88,6 +105,9 @@ class TodayViewController: UIViewController, NCWidgetProviding,UITableViewDelega
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var eventLabel: UILabel!
+    
+    
     // 日付を格納
     var modifiedDate:Date = Date()
     
@@ -107,6 +127,7 @@ class TodayViewController: UIViewController, NCWidgetProviding,UITableViewDelega
         
         LectureGet.fetchArticle(date: getday(format:"yyyy-MM-dd"),completion: { (articles) in
             self.arrayTimetableParse(lectures: articles)
+            self.schoolCalender(date: self.getday(format:"yyyy-MM-dd"))
             //print(articles)
             DispatchQueue.main.async {
                 self.tableView.reloadData()
@@ -124,14 +145,14 @@ class TodayViewController: UIViewController, NCWidgetProviding,UITableViewDelega
     func arrayTimetableParse(lectures:[Lecture]){
         // 配列の中身を消去しないと無限に配列の要素が増える
         self.articles.removeAll()
-        for i in 0..<6 {
+        for i in 1...6 {
             lectures.forEach{
-                if $0.period == i+1 {
+                if $0.period == i {
                     self.articles.append($0)
                 }
             }
-            if self.articles.count != i+1{
-                self.articles.append(Lecture(period: i+1,room: "----",lecture_name: "----",instructor: ""))
+            if self.articles.count != i{
+                self.articles.append(Lecture(period: i,room: "----",lecture_name: "----",instructor: ""))
             }
         }
     }
@@ -155,6 +176,7 @@ class TodayViewController: UIViewController, NCWidgetProviding,UITableViewDelega
             //print(articles)
             DispatchQueue.main.async {
                 self.tableView.reloadData()
+                self.schoolCalender(date: self.getday(format:"yyyy-MM-dd"))
             }
         })
     }
@@ -168,8 +190,10 @@ class TodayViewController: UIViewController, NCWidgetProviding,UITableViewDelega
             //print(articles)
             DispatchQueue.main.async {
                 self.tableView.reloadData()
+                self.schoolCalender(date: self.getday(format:"yyyy-MM-dd"))
             }
         })
+        
     }
     
     // 日付を引数で指定されたフォーマットに変換
@@ -182,8 +206,65 @@ class TodayViewController: UIViewController, NCWidgetProviding,UITableViewDelega
         return formatter.string(from: modifiedDate as Date)
     }
     
-    
+    // イベント情報の取得
+    func schoolCalender(date:String){
+        let requestUrl = "https://dev.api.twinte.net/v1/school-calender/"+date
+        
+        // URL生成
+        guard let url = URL(string: requestUrl) else {
+            // URL生成失敗
+            return
+        }
+        
+        // リクエスト生成
+        let request = URLRequest(url: url)
+        // APIを殴る
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { (data:Data?,
+            response:URLResponse?, error:Error?) in
+            // 通信完了後の処理
+            // エラーチェック
+            guard error == nil else {
+                // エラー表示
+                let alert = UIAlertController(title: "エラー",
+                                              message: error?.localizedDescription,
+                                              preferredStyle: UIAlertController.Style.alert)
+                print(alert)
+                return
+            }
+            
+            // JSONで返却されたデータをパースして格納する
+            guard let data = data else {
+                // データなし
+                return
+            }
+            
+            do {
+                // jsonのパース実施
+                let resultSet = try JSONDecoder().decode(OutputSchoolCalender.self, from: data)
+                
+                // 優先度：substituteDay > event > module
+                if let substituteDay = resultSet.substituteDay{
+                    self.eventLabel.text = "今日は\(substituteDay.change_to)曜日課です"
+                } else if let event = resultSet.event{
+                    self.eventLabel.text = "\(event.event_type) \(event.description)"
+                } else if let module = resultSet.module{
+                    self.eventLabel.text = "\(module)モジュール"
+                }else{
+                    self.eventLabel.text = ""
+                }
+            } catch let error {
+                print("## error: \(error)")
+            }
+        }
+        // 通信開始
+        task.resume()
+        
+    }
 }
+
+
+
 
 extension TodayViewController: UITableViewDataSource {
     
@@ -205,4 +286,5 @@ extension TodayViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return articles.count
     }
+    
 }
