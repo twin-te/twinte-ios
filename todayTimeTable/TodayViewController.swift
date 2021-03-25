7070f9cb13169ec2e37a867f6e1270656cea16d9
takonasu <piano88_viola_tako8@hotmail.co.jp>
2021-03-25 17:26:52 +0900

    add: ウィジェットの新API対応

--- todayTimeTable/TodayViewController.swift
+++ todayTimeTable/TodayViewController.swift
@@ -9,87 +9,207 @@
 import UIKit
 import NotificationCenter
 
-// 授業情報を格納
-struct Lecture: Codable {
-    let period: Int
-    let room: String
-    let lecture_name: String
-    let instructor: String
-    let formats: [String]
-}
 
-// イベント情報格納
-struct OutputSchoolCalender: Codable {
-    let module:String?
-    let event:event?
-    let substituteDay:substituteDay?
+// 新ウィジェットAPI用
+struct todayList: Codable {
+    let module:module?
+    let events:[event]
+    let courses:[eachCourse]
+    
+    struct module: Codable{
+        let module:String
+        let year:Int
+    }
     
     struct event: Codable{
+        let eventType:String
         let description:String
-        let event_type:String
+        // 存在しないことがある
+        let changeTo:String?
     }
     
-    struct substituteDay: Codable{
-        let change_to:String
+    struct eachCourse: Codable{
+        let year:Int
+        let course:course
+        
+        struct course: Codable {
+            let name:String
+            let methods: [String]
+            let schedules: [schedule]
+            
+            struct schedule: Codable {
+                let module:String
+                let day:String
+                let period:Int
+                let room:String
+            }
+        }
     }
 }
 
-struct LectureGet {
+// 最終的にウィジェットで使う授業情報
+struct ReturnObject{
+    let Lectures:[Lecture]
+    let description:String
+    let changeTo:String
+    let module:String
+}
+
+struct Lecture{
+    let name:String
+    let room:String
+    let methods:[String]
+    let period:Int
+}
+
+/// データ読み込み処理
+func fetchAPI(date:String,completion: @escaping (ReturnObject) -> Void) {
     
-    static func fetchArticle(date:String,completion: @escaping ([Lecture]) -> Swift.Void) {
-        
-        let requestUrl = "https://api.twinte.net/v1/timetables/?date="+date
-        
-        // URL生成
-        guard let url = URL(string: requestUrl) else {
-            // URL生成失敗
-            return
-        }
-        
-        // リクエスト生成
-        var request = URLRequest(url: url)
-        // UserDefaults のインスタンス
-        let userDefaults = UserDefaults(suiteName: "group.net.twinte.app")
-        if let stringCookie = userDefaults?.string(forKey: "stringCookie"){
-            // UserDefaultsからCookieを取得
-            request.setValue(stringCookie, forHTTPHeaderField: "Cookie")
-        }
-        // APIを殴る
-        let session = URLSession.shared
-        let task = session.dataTask(with: request) { (data:Data?,
-            response:URLResponse?, error:Error?) in
-            // 通信完了後の処理
-            // エラーチェック
-            guard error == nil else {
-                // エラー表示
-                let alert = UIAlertController(title: "エラー",
-                                              message: error?.localizedDescription,
-                                              preferredStyle: UIAlertController.Style.alert)
-                print(alert)
+    // 重複している時間帯は"重複しています"という授業を登録
+    // 最後に返すオブジェクトの中に入れる配列
+    var todayLectureListWithoutDuplicate:[Lecture] = []
+    
+    /// URLの生成
+    guard let url = URL(string: "https://api.dev.twinte.net/v3/timetable/"+date) else {
+        /// 文字列が有効なURLでない場合の処理
+        return
+    }
+    /// URLリクエストの生成
+    var request = URLRequest(url: url)
+    // UserDefaults のインスタンス
+    let userDefaults = UserDefaults(suiteName: "group.net.twinte.app")
+    if let stringCookie = userDefaults?.string(forKey: "stringCookie"){
+        // UserDefaultsからCookieを取得
+        request.setValue(stringCookie, forHTTPHeaderField: "Cookie")
+    }
+    /// URLにアクセス
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        if let data = data {    // データ取得チェック
+            let decorder = JSONDecoder()
+            guard let decodedResponse = try? decorder.decode(todayList.self, from: data) else {
+                print("Json decode エラー")
+                todayLectureListWithoutDuplicate.append(Lecture(name:"---",room:"未認証です",methods:[],period:1))
+                todayLectureListWithoutDuplicate.append(Lecture(name:"---",room:"Twin:teにログインしてください",methods:[],period:2))
+                todayLectureListWithoutDuplicate.append(Lecture(name:"---",room:"ログイン済みでこのエラーが出る場合は",methods:[],period:3))
+                todayLectureListWithoutDuplicate.append(Lecture(name:"---",room:"お手数ですが運営までご連絡ください。",methods:[],period:4))
+                for i in 5...6 {
+                    todayLectureListWithoutDuplicate.append(Lecture(name:"---",room:"",methods:[],period:i))
+                }
+                
+                completion(ReturnObject(Lectures:todayLectureListWithoutDuplicate,description:"",changeTo:"",module: ""))
                 return
             }
+            // 重複ありでとりあえず今日登録してる授業一覧を格納
+            var todayLectureList:[Lecture] = []
+            // 日課変更がない場合は指定された日の曜日を取得
+            // ある場合には後ほど格納
+            var changeTo:String = getWeekDate()
             
-            // JSONで返却されたデータをパースして格納する
-            guard let data = data else {
-                // データなし
-                return
-            }
+            // ウィジェットに表示する用
+            var displayDescription:String = ""
+            var displayChangeTo:String = ""
+            var displayModule:String = ""
             
-            do {
-                // jsonのパース実施
-                let resultSet = try JSONDecoder().decode([Lecture].self, from: data)
-                //print(resultSet)
-                completion(resultSet)
-            } catch let error {
-                print("## error: \(error)")
-                // ログインしていない場合にこのエラーが出る（jsonで返ってこないので）
-                let resultSet = [Lecture(period: 1,room: "未認証です",lecture_name: "Twin:teにログインしてください",instructor: "",formats: []),Lecture(period: 2,room: "未認証です",lecture_name: "ここに時間割が表示されます！",instructor: "",formats: [])]
-                completion(resultSet)
+            // print(decodedResponse.module.module)
+            if decodedResponse.events.count > 0 {
+                displayDescription = decodedResponse.events[0].description
+                // 日課変更がある場合にはchangeToに格納する
+                if decodedResponse.events[0].changeTo != nil{
+                    changeTo = decodedResponse.events[0].changeTo!
+                    displayChangeTo = convertDayEnglishToJapanese(day: decodedResponse.events[0].changeTo!)
+                }
             }
+            // モジュールが記載されない時（冬休み）があるのでその対処
+            if(decodedResponse.module != nil){
+                for element in decodedResponse.courses{
+                    displayModule = convertModuleEnglishToJapanese(module: decodedResponse.module!.module)
+                    // 今日のモジュールかつ、今日の曜日(日課変更の場合は変更後の曜日)のもの
+                    let newScheduleArray = element.course.schedules.filter{$0.day == changeTo && $0.module == decodedResponse.module!.module }
+                    
+                    newScheduleArray.forEach{
+                        todayLectureList.append(Lecture(name:element.course.name,room:$0.room,methods:element.course.methods,period:$0.period))
+                    }
+                }
+            }
+            // 授業時間順にソート
+            todayLectureList.sort(by: {$0.period < $1.period})
+            
+            for i in 1...6 {
+                let tmpArray = todayLectureList.filter{$0.period == i}
+                // その時間の授業が2つ以上ある場合
+                if tmpArray.count > 1 {
+                    todayLectureListWithoutDuplicate.append(Lecture(name:"授業が重複しています",room:"",methods:[],period:i))
+                }else if tmpArray.count == 0{ // その時限に授業が登録されていない場合
+                    todayLectureListWithoutDuplicate.append(Lecture(name:"---",room:"---",methods:[],period:i))
+                }else{
+                    todayLectureListWithoutDuplicate.append(tmpArray[0])
+                }
+            }
+            completion(ReturnObject(Lectures: todayLectureListWithoutDuplicate, description: displayDescription, changeTo:displayChangeTo,module: displayModule))
+            
+        } else {
+            /// データが取得できなかった場合の処理
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
         }
-        // 通信開始
-        task.resume()
-        
+    }.resume()
+}
+
+
+// ここに格納される日付がAPIから取得する対象の日付
+var modifiedDate:Date = Date()
+
+// modifiedDateの曜日をSun,Mon... 形式の文字列で返す
+func getWeekDate()->String{
+    let formatter = DateFormatter()
+    formatter.locale = Locale(identifier: "en")
+    formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "E", options: 0, locale: Locale.current)
+    return formatter.string(from: modifiedDate)
+}
+
+
+func convertModuleEnglishToJapanese(module:String)->String{
+    switch module {
+    case "SpringA":
+        return "春A"
+    case "SpringB":
+        return "春AB"
+    case "SpringC":
+        return "春C"
+    case "SummerVacation":
+        return "夏休み"
+    case "FallA":
+        return "秋A"
+    case "FallB":
+        return "秋B"
+    case "FallC":
+        return "秋C"
+    case "SpringVacation":
+        return "春休み"
+    default:
+        return ""
+    }
+}
+
+func convertDayEnglishToJapanese(day:String)->String{
+    switch day {
+    case "Sun":
+        return "日曜"
+    case "Mon":
+        return "月曜"
+    case "Tue":
+        return "火曜"
+    case "Wed":
+        return "水曜"
+    case "Thu":
+        return "木曜"
+    case "Fri":
+        return "金曜"
+    case "Sat":
+        return "土曜"
+    case "":
+        return ""
+    default:
+        return "特殊"
     }
 }
 
@@ -117,8 +237,6 @@ class TodayViewController: UIViewController, NCWidgetProviding,UITableViewDelega
     @IBOutlet weak var rightButton: UIButton!
     @IBOutlet weak var leftButton: UIButton!
     
-    // 日付を格納
-    var modifiedDate:Date = Date()
     
     fileprivate var articles: [Lecture] = []
     
@@ -134,9 +252,8 @@ class TodayViewController: UIViewController, NCWidgetProviding,UITableViewDelega
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
         
-        LectureGet.fetchArticle(date: getday(format:"yyyy-MM-dd"),completion: { (articles) in
-            self.arrayTimetableParse(lectures: articles)
-            self.schoolCalender(date: self.getday(format:"yyyy-MM-dd"))
+        fetchAPI(date: getday(format:"yyyy-MM-dd"),completion: { (articles) in
+            self.articles = articles.Lectures
             //print(articles)
             DispatchQueue.main.async {
                 self.tableView.reloadData()
@@ -150,21 +267,6 @@ class TodayViewController: UIViewController, NCWidgetProviding,UITableViewDelega
         completionHandler(NCUpdateResult.newData)
     }
     
-    // 授業の時間と配列のindexを対応させて格納する
-    func arrayTimetableParse(lectures:[Lecture]){
-        // 配列の中身を消去しないと無限に配列の要素が増える
-        self.articles.removeAll()
-        for i in 1...6 {
-            lectures.forEach{
-                if $0.period == i {
-                    self.articles.append($0)
-                }
-            }
-            if self.articles.count != i{
-                self.articles.append(Lecture(period: i,room: "----",lecture_name: "----",instructor: "",formats: []))
-            }
-        }
-    }
     
     func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode,
                                           withMaximumSize maxSize: CGSize) {
@@ -180,18 +282,11 @@ class TodayViewController: UIViewController, NCWidgetProviding,UITableViewDelega
     @IBAction func leftButton(_ sender: Any) {
         modifiedDate = Calendar.current.date(byAdding: .day, value: -1, to: modifiedDate)!
         dateLabel.text = self.getday(format:"MM/dd(EEE)")
-        LectureGet.fetchArticle(date: self.getday(format:"yyyy-MM-dd"),completion: { (articles) in
-            DispatchQueue.main.async {
-                self.rightButton.isEnabled = false
-                self.leftButton.isEnabled = false
-            }
-            self.arrayTimetableParse(lectures: articles)
-            //print(articles)
-            self.schoolCalender(date: self.getday(format:"yyyy-MM-dd"))
+        fetchAPI(date: self.getday(format:"yyyy-MM-dd"),completion: { (articles) in
+            self.articles = articles.Lectures
+            self.updateWidgetTexts(element: articles)
             DispatchQueue.main.async {
                 self.tableView.reloadData()
-                self.rightButton.isEnabled = true
-                self.leftButton.isEnabled = true
             }
         })
     }
@@ -200,22 +295,13 @@ class TodayViewController: UIViewController, NCWidgetProviding,UITableViewDelega
     @IBAction func rightButton(_ sender: Any) {
         modifiedDate = Calendar.current.date(byAdding: .day, value: 1, to: modifiedDate)!
         dateLabel.text = self.getday(format:"MM/dd(EEE)")
-        LectureGet.fetchArticle(date: self.getday(format:"yyyy-MM-dd"),completion: { (articles) in
-            DispatchQueue.main.async {
-                self.rightButton.isEnabled = false
-                self.leftButton.isEnabled = false
-            }
-            self.arrayTimetableParse(lectures: articles)
-            //print(articles)
-            self.schoolCalender(date: self.getday(format:"yyyy-MM-dd"))
+        fetchAPI(date: self.getday(format:"yyyy-MM-dd"),completion: { (articles) in
+            self.articles = articles.Lectures
+            self.updateWidgetTexts(element: articles)
             DispatchQueue.main.async {
                 self.tableView.reloadData()
-                self.rightButton.isEnabled = true
-                self.leftButton.isEnabled = true
             }
-            
         })
-        
     }
     
     // 日付を引数で指定されたフォーマットに変換
@@ -227,64 +313,21 @@ class TodayViewController: UIViewController, NCWidgetProviding,UITableViewDelega
         return formatter.string(from: modifiedDate as Date)
     }
     
-    // イベント情報の取得
-    func schoolCalender(date:String){
-        let requestUrl = "https://api.twinte.net/v1/school-calender/"+date
-        
-        // URL生成
-        guard let url = URL(string: requestUrl) else {
-            // URL生成失敗
-            return
-        }
-        
-        // リクエスト生成
-        let request = URLRequest(url: url)
-        // APIを殴る
-        let session = URLSession.shared
-        let task = session.dataTask(with: request) { (data:Data?,
-            response:URLResponse?, error:Error?) in
-            // 通信完了後の処理
-            // エラーチェック
-            guard error == nil else {
-                // エラー表示
-                let alert = UIAlertController(title: "エラー",
-                                              message: error?.localizedDescription,
-                                              preferredStyle: UIAlertController.Style.alert)
-                print(alert)
-                return
-            }
-            
-            // JSONで返却されたデータをパースして格納する
-            guard let data = data else {
-                // データなし
-                return
-            }
-            
-            do {
-                // jsonのパース実施
-                let resultSet = try JSONDecoder().decode(OutputSchoolCalender.self, from: data)
-                
-                DispatchQueue.main.async {
-                    // 優先度：substituteDay > event > module
-                    if let substituteDay = resultSet.substituteDay{
-                        self.eventLabel.text = "今日は\(substituteDay.change_to)曜日課です"
-                    } else if let event = resultSet.event{
-                        self.eventLabel.text = "\(event.event_type) \(event.description)"
-                    } else if let module = resultSet.module{
-                        self.eventLabel.text = "\(module)"
-                    }else{
-                        self.eventLabel.text = ""
-                    }
-                }
-            } catch let error {
-                print("## error: \(error)")
+    func updateWidgetTexts(element:ReturnObject){
+        DispatchQueue.main.async {
+            // 優先度：substituteDay > event > module
+            if element.changeTo != ""{
+                self.eventLabel.text = "今日は\(element.changeTo)日課です"
+            }else if element.description != ""{
+                self.eventLabel.text = element.description
+            }else{
+                self.eventLabel.text = element.module
             }
         }
-        // 通信開始
-        task.resume()
-        
     }
 }
+
+
 
 // ダークモード判定
 extension UITraitCollection {
@@ -309,7 +352,7 @@ extension TodayViewController: UITableViewDataSource {
         // セルに表示する値を設定する
         let article = articles[indexPath.row]
         //cell.textLabel?.text = article.lecture_name
-        cell.lectureNameLabel?.text = article.lecture_name
+        cell.lectureNameLabel?.text = article.name
         cell.lectureRoomLabel?.text = article.room
         cell.lecturePeriodLabel?.text = article.period.description
         cell.lecturePeriodLabel?.textColor = UIColor.white
@@ -325,8 +368,8 @@ extension TodayViewController: UITableViewDataSource {
             cell.faceToFace.image = UIImage(named: "disable-light-face-to-face")
         }
         // 授業フォーマットに記載がある場合
-        if(article.formats.count != 0){
-            article.formats.forEach{
+        if(article.methods.count != 0){
+            article.methods.forEach{
                 // ダークモード判定
                 if (UITraitCollection.isDarkMode){
                     switch $0 {
