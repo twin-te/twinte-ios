1c09d67b22a633cffc209a96e6ba883c2e22119c
TowaSakurada <towatowa3939@icloud.com>
2025-11-14 18:59:31 +0900

    UNNotificationPresent ・.alertがiOS14以降からdeprecatedになりbannerとlistに分けられた。 →分けた。twin:teのターゲットがiOS14以降なのでバージョン分岐はしなかった。

--- Twinte/AppDelegate.swift
+++ Twinte/AppDelegate.swift
@@ -44,6 +44,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
                                 willPresent notification: UNNotification,
                                 withCompletionHandler completionHandler:
                                 @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.alert, .sound])
+        completionHandler([.banner, .list, .sound])
     }
 }
